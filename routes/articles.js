a80f769ca4ac273071de147bdf9ad31c3594d58a
Chris Naismith <christophernaismith@gmail.com>
2019-02-08 14:44:46 -0500

    Edit and Delete for articles

--- routes/articles.js
+++ routes/articles.js
@@ -20,6 +20,30 @@ router.get('/create', function(req, res, next) {
   res.render('article-create', {});
 });
 
+router.get('/edit/:id', function(req, res, next) {
+  const id = req.params.id;
+
+  Article.findById(id).then(article => {
+    // res.json(article);
+    res.render('article-edit', { article });
+  });
+});
+
+router.post('/edit/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  Article.findByIdAndUpdate(id, req.body, { new: true }).then(article => {
+    res.redirect('/articles');
+    // res.render('article-edit', { article });
+  });
+});
+
+router.get('/delete/:id', (req, res, next) => {
+  Article.findByIdAndRemove(req.params.id).then(article => {
+    res.redirect('/articles');
+  });
+});
+
 router.post('/create', function(req, res, next) {
   const newArticle = new Article({ ...req.body, date: new Date() });
   newArticle.save(function(err) {
